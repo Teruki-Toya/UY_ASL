7f42e999c72eb69e5d90280315c16fc9bc860bfe
TerukiToya <t.toya@yamanashi.ac.jp>
2024-10-24 15:13:04 +0900

    フレーム処理は時間関係の関数群にあったほうが便利そうなため

--- analTemp.py
+++ analTemp.py
@@ -47,3 +47,31 @@ def sigsTimeSyncronize(sig1, sig0):
     sig1r = np.concatenate([sig1[len(sig1)+d:], sig1[0:len(sig1)+d]])
 
   return sig1r
+
+## シフトしながら短時間フレームに分割 -----------------------------
+def specByFrm(x, fs, winSize, shiftSize = -9999):
+  """
+  < 入力 >
+    x:  分析対象の時間波形（系列データ）
+    fs: サンプリング周波数 [Hz]
+    winSize: 窓長（フレームサンプル数）
+    [shiftSize: フレームシフト長 --> デフォルトは窓長の半分]
+  < 出力 >
+    x_frm:  フレームごとに分割されたデータ [フレーム数 × 窓長]
+  """
+  
+  # 窓のシフト長
+  if shiftSize < 0:
+    shiftSize = int(winSize / 2)
+  
+  # 総フレーム数
+  N_frame = int(np.floor((len(x) - (winSize - shiftSize)) / shiftSize))
+  
+  # フレームごとに分割した波形行列 [フレーム数 × 窓長]
+  x_frm = np.zeros((N_frame, winSize))
+  for frame in range(N_frame):
+    offset = shiftSize * frame  # フレームをシフトしながら
+    s = x[offset : offset + winSize]
+    x_frm[frame, :] = s
+  
+  return x_frm
